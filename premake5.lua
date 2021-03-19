df00eb608df152739055008f9db85650b6f676b6
Katamithetaka <47742910+Katamithetaka@users.noreply.github.com>
2021-03-19 14:58:41 +0100

    Update premake5.lua
    Removed options related to windows because I will use Win32 Api for it

--- premake5.lua
+++ premake5.lua
@@ -42,29 +42,6 @@ project "GLFW"
 			"_GLFW_X11"
 		}
 
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
-
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"

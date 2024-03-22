75a45dc64e1481647124f48cdb3baf665f475a22
Katamithetaka <katamithetaka@gmail.com>
2024-03-22 15:01:58 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -9,65 +9,98 @@ project "GLFW"
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
-		"src/platform.c",
-		"src/vulkan.c",
-		"src/window.c",
+		 "include/GLFW/glfw3.h",
+                 "include/GLFW/glfw3native.h",
+	
+                 "src/internal.h", 
+		 "src/platform.h",
+		 "src/mappings.h",
+                 "src/context.c",
+		 "src/init.c",
+		 "src/input.c",
+		 "src/monitor.c",
+	 	 "src/platform.c", 
+		 "src/vulkan.c",
+		 "src/window.c",
+                 "src/egl_context.c",
+		 "src/osmesa_context.c",
+	
+		 "src/null_platform.h",
+		 "src/null_joystick.h"
+                 "src/null_init.c",
+		 "src/null_monitor.c",
+		 "src/null_window.c",
+		 "src/null_joystick.c"
 	}
 
 	filter "system:linux"
 		pic "On"
 
 		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_module.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
+		if os.getenv("WAYLAND") then
+			defines
+			{
+				"_GLFW_WAYLAND"
+			}
+	
+			files
+			{
+				"src/posix_time.h",
+				"src/posix_thread.h",
+				"src/posix_module.c",
+				"src/posix_time.c",
+				"src/posix_thread.c",
+				"src/wl_platform.h",
+				"src/xkb_unicode.h",
+				"src/wl_init.c",
+	                        "src/wl_monitor.c",
+				"src/wl_window.c",
+				"src/xkb_unicode.c",
+				"src/linux_joystick.h",
+				"src/linux_joystick.c"
+			}
+		else
+			defines
+			{
+				"_GLFW_X11"
+			}
+			files
+			{
+				"src/posix_time.h",
+				"src/posix_thread.h",
+				"src/posix_module.c",
+				"src/posix_time.c",
+				"src/posix_thread.c",
+				"src/x11_platform.h",
+				"src/xkb_unicode.h", 
+				"src/x11_init.c",
+                                "src/x11_monitor.c",
+				"src/x11_window.c",
+				"src/xkb_unicode.c",
+                                "src/glx_context.c",
+				"src/linux_joystick.h",
+				"src/linux_joystick.c"
+			}
+		end
 
 	filter "system:macosx"
 		pic "On"
 
 		files
 		{
+			"src/cocoa_time.h",
+			"src/cocoa_time.c",
+			"src/posix_thread.h",
+			"src/posix_module.c",
+			"src/posix_thread.c",
+
+			"src/cocoa_platform.h", 
+			"src/cocoa_joystick.h",
 			"src/cocoa_init.m",
+	                "src/cocoa_joystick.m",
 			"src/cocoa_monitor.m",
 			"src/cocoa_window.m",
-			"src/cocoa_joystick.m",
-			"src/cocoa_time.c",
-			"src/nsgl_context.m",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/osmesa_context.c",
-			"src/egl_context.c"
+			"src/nsgl_context.m"
 		}
 
 		defines
@@ -80,16 +113,18 @@ project "GLFW"
 
 		files
 		{
+			"src/win32_platform.h", 
+			"src/win32_joystick.h", 
 			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_module.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
+                        "src/win32_joystick.c", 
+			"src/win32_monitor.c", 
 			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+                        "src/wgl_context.c",
+			"src/win32_time.h", 
+			"src/win32_thread.h",
+			"src/win32_module.c",
+			"src/win32_time.c",
+			"src/win32_thread.c"
 		}
 
 		defines 
